48476c27c2c60ef03c0817d11d71c8ec96d9809c
wduda <wojciech.duda@me.com>
2021-02-20 17:47:55 +0100

    made sure optionspanel reads the right class key

--- src/OptionsPanel.lua
+++ src/OptionsPanel.lua
@@ -417,10 +417,10 @@ function OptionsPanel:AddItems()
             counter = counter + 1;
         end
     end
-    
+
     -- add the race specific travel skill for the character
     if (playerAlignment == Turbine.Gameplay.Alignment.FreePeople) then
-        self:AddSkillItemForEnabling(counter, racialLocations:IdAtIndex(self.raceKey), racialLocations:LabelAtIndex(self.raceKey));
+        self:AddSkillItemForEnabling(counter, racialLocations:IdAtIndex(settings.playerRaceKey), racialLocations:LabelAtIndex(settings.playerRaceKey));
         counter = counter + 1;
     end
 
