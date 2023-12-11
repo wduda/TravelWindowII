a945aeb25041a137fae614974a2e260d8a9354ef
whiterabbit963 <whiterabbit963@proton.me>
2023-12-10 22:19:37 -0500

    fixes #135 in EN

--- src/TravelCaroTab.lua
+++ src/TravelCaroTab.lua
@@ -100,7 +100,7 @@ function TravelCaroTab:SetItems()
             -- apply skill type filter if set in options
             if (hasbit(Settings.filters, bit(TravelShortcuts[i]:GetTravelType()))) then
                 -- make sure skill is trained, lookup by ingame name
-                if (TravelWindow:FindSkill(TravelShortcuts[i]:GetName())) then
+                if (TravelWindow:FindSkill(TravelShortcuts[i])) then
                     table.insert(self.shortcuts, TravelShortcuts[i]);
                 end
             end
--- src/TravelPulldownTab.lua
+++ src/TravelPulldownTab.lua
@@ -115,7 +115,7 @@ function TravelPulldownTab:SetItems()
             -- apply skill type filter if set in options
             if (hasbit(Settings.filters, bit(TravelShortcuts[i]:GetTravelType()))) then
                 -- make sure skill is trained, lookup by ingame name
-                if (TravelWindow:FindSkill(TravelShortcuts[i]:GetName())) then
+                if (TravelWindow:FindSkill(TravelShortcuts[i])) then
                     self.pulldown:AddItem(TravelShortcuts[i], shortcutIndex, i);
                     shortcutIndex = shortcutIndex + 1;
                 end
