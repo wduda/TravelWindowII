9b447fd3c620954d6d55cf4e0e8d82551d400114
whiterabbit963 <whiterabbit963@proton.me>
2023-12-20 13:53:28 -0500

    hide options window on escape and ui_toggle

--- src/TravelWindow.lua
+++ src/TravelWindow.lua
@@ -173,6 +173,7 @@ function TravelWindow:Constructor()
     self.KeyDown = function(sender, args)
         if (args.Action == Turbine.UI.Lotro.Action.Escape) then
             self:SetVisible(false);
+            self.optionsWindow:SetVisible(false);
             self:CloseOptions();
             self:CloseGondorMap();
             self:CloseMoorMap();
@@ -188,6 +189,7 @@ function TravelWindow:Constructor()
                 self.currentVisState = self:IsVisible();
                 self.hidden = true;
                 self:SetVisible(false);
+                self.optionsWindow:SetVisible(false);
                 self.ToggleButton:SetVisible(false);
             else
                 self.hidden = false;
