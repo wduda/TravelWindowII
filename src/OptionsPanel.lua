7797af4a3662a2ee6c0692fab0801a1c611b5f2a
whiterabbit963 <whiterabbit963@proton.me>
2024-09-26 16:28:44 -0400

    fixup multiline height calculations for acquire skill info

--- src/OptionsPanel.lua
+++ src/OptionsPanel.lua
@@ -4,12 +4,70 @@ import "Turbine.UI.Lotro"
 import "TravelWindowII.src.extensions"
 import "TravelWindowII.src.utils.BitOps"
 
-FindTreeNode = class(Turbine.UI.TreeNode)
+-- FontMetric is copied from Garan's MoorMap
+FontMetric = class( Turbine.UI.Label );
+function FontMetric:Constructor()
+	Turbine.UI.Control.Constructor( self );
+	self.Text=Turbine.UI.Label();
+	self.Text:SetParent(self);
+	self.VScroll=Turbine.UI.Lotro.ScrollBar();
+	self.VScroll:SetOrientation(Turbine.UI.Orientation.Vertical);
+	self.VScroll:SetParent(self);
+	self.Text:SetVerticalScrollBar(self.VScroll);
+	self.HScroll=Turbine.UI.Lotro.ScrollBar();
+	self.HScroll:SetOrientation(Turbine.UI.Orientation.Horizontal);
+	self.HScroll:SetParent(self);
+	self.Text:SetHorizontalScrollBar(self.HScroll);
+	self.fontHeight=12;
+	self.SetFont=function(sender,font)
+		self.Text:SetFont(font);
+		if Turbine.UI.Lotro.FontInfo[font]~=nil then
+			self.fontHeight=Turbine.UI.Lotro.FontInfo[font].size
+		else
+			self.fontHeight=self:GetHeight()
+		end
+	end
+	self.GetTextWidth=function(sender,text,height)
+		if height==nil or height<self.fontHeight then height=self.fontHeight end
+		local width=0
+		if text~=nil and text~="" then
+			self.Text:SetHeight(height);
+			self.Text:SetMultiline(false);
+			width=string.len(text);
+			self.Text:SetText(text);
+			self.Text:SetWidth(width);
+			while self.HScroll:IsVisible() do
+				width=width+1;
+				self.Text:SetWidth(width);
+			end
+		end
+		return width;
+	end
+	self.GetTextHeight=function(sender,text,width)
+		if width~=nil then
+			-- since we don't actually have a minimum width for a single character, use the point size as a good estimate - this is needed to prevent possible infinite loop issues
+			if width<self.fontHeight then width=self.fontHeight end
+			self.Text:SetWidth(width)
+		end
+		local height=self.fontHeight
+		self.Text:SetMultiline(true);
+		if text~=nil and text~="" then
+			self.Text:SetText(text);
+			self.Text:SetHeight(height);
+			while self.HScroll:IsVisible() or self.VScroll:IsVisible() do
+				height=height+1;
+				self.Text:SetHeight(height);
+			end
+		end
+		return height;
+	end
+end
 
+FindTreeNode = class(Turbine.UI.TreeNode)
 function FindTreeNode:Constructor(width, text, top)
 	Turbine.UI.TreeNode.Constructor(self)
 
-    self:SetSize(width, 20)
+    local height = 16
     self.expanded = false
 
     if top then
@@ -18,12 +76,20 @@ function FindTreeNode:Constructor(width, text, top)
         self.icon:SetSize(16, 16)
         self.icon:SetBackground(0x41007E27)
         self.icon:SetMouseVisible(false)
+    else
+        local fm = FontMetric()
+        -- NOTE: if self.label sets a non-default Font
+        -- update fm with that font
+        height = fm:GetTextHeight(text, width)
     end
+
+    self:SetSize(width, height + 8)
 
     self.label = Turbine.UI.Label()
     self.label:SetParent(self)
+    self.label:SetTop(2)
     self.label:SetLeft(self.icon and 27 or 10)
-    self.label:SetSize(width, 16)
+    self.label:SetSize(width, height)
     self.label:SetMouseVisible(false)
     self.label:SetTextAlignment(Turbine.UI.ContentAlignment.TopLeft)
     self.label:SetMultiline(true)
@@ -910,9 +976,6 @@ function OptionsPanel:AddFindTreeShortcuts()
                 local text, lines = shortcut:GetAcquireText()
                 local child = node:GetChildNodes()
                 local infoNode = FindTreeNode(width - 20, text, false)
-                infoNode:SetHeight(lines * 14 + 4)
-                infoNode.label:SetTop(2)
-                infoNode.label:SetHeight(lines * 14)
                 child:Add(infoNode)
             end
         end
