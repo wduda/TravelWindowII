948e2db49c94e8b072f5ace735c75d52937e69a8
whiterabbit963 <whiterabbit963@proton.me>
2025-04-08 00:10:41 -0400

    incorporate Travel Skill data generated from game files

--- src/__init__.lua
+++ src/__init__.lua
@@ -8,6 +8,7 @@ import "TravelWindowII.src.IndexedDictionary"
 import "TravelWindowII.src.TravelDictionary"
 import "TravelWindowII.src.SkillData"
 import "TravelWindowII.src.LocaleStrings"
+import "TravelWindowII.src.LocaleData"
 import "TravelWindowII.src.TravelShortcut";
 import "TravelWindowII.src.OptionsWindow";
 import "TravelWindowII.src.TravelButton";
