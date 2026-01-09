f1267884f5cae36cd89a8cc043b7dcbe6bd7a801
Wojciech Duda <wojciech.duda@zalando.de>
2026-01-09 12:29:32 +0100

    refactoring SettingsMenu into two classes - one for Settings and one for the menu itself

--- src/__init__.lua
+++ src/__init__.lua
@@ -1,8 +1,8 @@
-import "TravelWindowII.src.OldTurbineUtil.Class";
-import "TravelWindowII.src.TravelWindow";
-import "TravelWindowII.src.TravelListTab";
-import "TravelWindowII.src.TravelGridTab";
-import "TravelWindowII.src.TravelCaroTab";
+import "TravelWindowII.src.OldTurbineUtil.Class"
+import "TravelWindowII.src.TravelWindow"
+import "TravelWindowII.src.TravelListTab"
+import "TravelWindowII.src.TravelGridTab"
+import "TravelWindowII.src.TravelCaroTab"
 import "TravelWindowII.src.TravelPulldownTab"
 import "TravelWindowII.src.TravelMapTab"
 import "TravelWindowII.src.IndexedDictionary"
@@ -10,11 +10,12 @@ import "TravelWindowII.src.TravelDictionary"
 import "TravelWindowII.src.SkillData"
 import "TravelWindowII.src.LocaleStrings"
 import "TravelWindowII.src.LocaleData"
-import "TravelWindowII.src.TravelShortcut";
-import "TravelWindowII.src.OptionsWindow";
-import "TravelWindowII.src.TravelButton";
+import "TravelWindowII.src.TravelShortcut"
+import "TravelWindowII.src.OptionsWindow"
+import "TravelWindowII.src.TravelButton"
+import "TravelWindowII.src.SettingsManager"
+import "TravelWindowII.src.SettingsMenu"
+import "TravelWindowII.src.OptionsPanel"
+import "TravelWindowII.src.ChangelogData"
+import "TravelWindowII.src.UpdateNotificationWindow"
 -- import "TravelWindowII.src.TravelTests";
-import "TravelWindowII.src.SettingsMenu";
-import "TravelWindowII.src.OptionsPanel";
-import "TravelWindowII.src.ChangelogData";
-import "TravelWindowII.src.UpdateNotificationWindow";
